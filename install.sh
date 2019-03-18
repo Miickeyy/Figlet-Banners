49b886556d4b0f54917f637cf367098026c32529
unknown <miigue.hernandez.98@gmail.com>
2019-03-18 03:34:46 -0600

    subiendo archivos en github

--- install.sh
+++ install.sh
@@ -1,28 +1,18 @@
 #!/bin/sh
 
 clear
-#CREADO EL 24/MARZO/2019
 cd 
-echo "\Cesar Hack Gray"
-figlet  "Cesar Hack Gray"
+echo "\033[1;93m#script por Mickey"
+figlet  "Cesar Hack gray"
 echo ""
-pkg update -y && pkg upgrade -y
-echo "$(tput setaf 5) Subscribete al < Canal De YOUTUBE Para más videos de Termux.! Cesar Hack Gray...!"
+pkg update -y
+pkg upgrade -y
+pkg install git
 
-echo -e '\e[0;36mDependencias instaladas CORRECTAMENTE\e[0m'
- read -r -p 'Presione ENTER para continuar Termux esta Actualizado'
- echo "."
-sleep 1
 clear
-echo ".."
-sleep 1
-clear
-echo "..................................."
-sleep 1
-clear
-echo "..........................................................."
-sleep 1
-clear
+
+pkg install fish -y
+echo "$(tput setaf 5) Suscríbete <Canal Cesar aHack Gray de  YouTube Para más videos de Termux "
 echo ""
 echo "$(tput setaf 10) Instalando FIGLET......."
 pkg install figlet -y
@@ -38,36 +28,23 @@ figlet -f Bubble "Instalando NodeJs"
 pkg install nodejs -y
 echo ""
 echo "$(tput setaf 5) Instalando FIGLET-cli......."
-npm install figlet
+npm install figlet -y
 
 figlet -f Bubble "Instalando figlet CLI"
-npm install -g figlet-cli
+npm install -g figlet-cli -y
 echo ""
 echo "\033[1;91m"
 
-cowsay "FINALIZADA"
+cowsay "Finished"
 
 figlet -f small "Done!"
 
-echo "$(tput setaf 10) instalación exitosa [DONE!]"
+echo "$(tput setaf 10) Instalación exitosa [HECHO!]"
 echo "|-----------------------------|"
-echo "$(tput setaf 3) Subscribete al canal de Cesar Hack Gray.."
+echo "$(tput setaf 3) Subscribete Cesar Hack Gray.."
 echo "|-----------------------------|"
-echo "."
-sleep 1
-clear
-echo ".."
-sleep 1
-clear
-echo "..................................."
-sleep 1
-clear
-echo "..........................................................."
-sleep 1
-echo "Ahora ejecuta esta herramienta con..! bash Lzfiglet .script echo por Mickey!."
-cd
+echo ""
+echo "Ahora ejecuta esta herramienta con Figlet ".."
 cd Lzyfigt
-mv Lzyfigt $PREFIX/bin
-mv install.sh $PREFIX/bin
-cd /data/data/com.termux/files/usr/bin; chmod +x 777 install.sh
-cd /data/data/com.termux/files/usr/bin; chmod +x 777 Lzyfigt
+
+
